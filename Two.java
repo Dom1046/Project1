19199ab36b8f13c09d8a48cff4aba7c33867390b
Dom1046 <rkrkwnj1046@gmail.com>
2024-06-21 18:13:09 +0900

    프로젝트 연습

---
+++ Two.java
@@ -0,0 +1,15 @@
+package Project1;
+
+import java.util.Scanner;
+
+public class Two {
+	public static void main(String[] args) {
+		Scanner Id = new Scanner(System.in);
+		
+		int x= Id.nextInt();
+		int y= Id.nextInt();
+		int sum = x+y;
+        System.out.println(sum);
+	
+	}
+}
